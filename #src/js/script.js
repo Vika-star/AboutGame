d3a33740c54c5a74c35fad8fd38bd0a1c9c0c72b
Diwonys <Diwonusojo71828@gmail.com>
2021-11-08 18:59:37 +0300

    Telegram Notification added

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,7 +2,7 @@
 const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
   const bodyMenu = document.querySelector('.menu__body');
-  iconMenu.addEventListener("click", function(params) {
+  iconMenu.addEventListener("click", function (params) {
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     bodyMenu.classList.toggle('_active');
@@ -28,44 +28,67 @@ const isMobile = {
     return navigator.userAgent.match(/IEMobile/i);
   },
   any: function () {
-    return(isMobile.Android()||
-          isMobile.BlackBerry()||
-          isMobile.iOS()||
-          isMobile.Opera()||
-          isMobile.Windows()
+    return (isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
     )
   }
 };
 
-if(isMobile.any()){
+if (isMobile.any()) {
   document.body.classList.add('_touch');
 
   let menuArrows = document.querySelectorAll('.menu__arrow');
-  
-  if(menuArrows.length > 0){
+
+  if (menuArrows.length > 0) {
     for (let index = 0; index < menuArrows.length; index++) {
-      
+
       const menuArrow = menuArrows[index];
-      
+
       menuArrow.addEventListener('click', onMenuArrowClick);
-      
+
       function onMenuArrowClick(e) {
-          menuArrow.parentElement.classList.toggle('_active-menu');        
+        menuArrow.parentElement.classList.toggle('_active-menu');
       }
     }
   }
-}else{
+} else {
   document.body.classList.add('_pc');
 }
 
+let telegramNotificationButton = document.getElementById('telegram-notification');
+telegramNotificationButton.addEventListener('click', function () {
+  let nameInput = document.getElementById('name');
+  let emailInput = document.getElementById('email');
+  let messageText = document.getElementById('messageText');
 
+  if (IsValidInput(nameInput) && IsValidInput(emailInput) && IsValidInput(messageText)) {
+    let text = `name: ${nameInput.value} email: ${emailInput.value} ${messageText.value}`;
+    sendOpinion(text);
+  }
+});
 
+function IsValidInput(input)
+{
+  return input.validity.valid;
+}
 
+function sendOpinion(str) {
+  var xmlHttp = new XMLHttpRequest();
+  var token = "2120820400:AAEowfh5hX8IerqvBcB13u6ozdqEIfHJfeQ";
+  var chat_id = 650663893;
+  xmlHttp.open("GET", 'https://api.telegram.org/bot' + token
+    + '/sendMessage?text=' + str
+    + '&chat_id=' + chat_id, false); // false for synchronous request
+  xmlHttp.send(null);
+}
 
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if(menuLinks.length > 0){
-  
-  menuLinks.forEach(menuLink =>{
+if (menuLinks.length > 0) {
+
+  menuLinks.forEach(menuLink => {
     menuLink.addEventListener('click', onMenuLinkClick);
   });
 
@@ -77,7 +100,7 @@ if(menuLinks.length > 0){
       // if you need fixed menu
       // const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
-    
+
 
       window.scrollTo({
         top: gotoBlockValue,
@@ -194,7 +217,7 @@ const swiper = new Swiper('.swiper', {
       slidesPerView: 1,
       spaceBetween: 25
     },
-    575:{
+    575: {
       slidesPerView: 1,
       spaceBetween: 25
     },
@@ -207,197 +230,18 @@ const swiper = new Swiper('.swiper', {
       slidesPerView: 3,
       spaceBetween: 40
     },
-    1366:{
+    1366: {
       slidesPerView: 1.7,
       spaceBetween: 40
     },
-    1630:{
+    1630: {
       slidesPerView: 2,
       spaceBetween: 40
     },
-    2100:{
+    2100: {
       slidesPerView: 2.5,
       spaceBetween: 40
     },
 
   }
 });
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   // loop: true,
-
-//   // // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-    
-//     clickable: true,
-//     dynamicBullets: true,
-//     //Numiration
-//     // renderBullet: function (index, className)
-//     // {
-//     // return '<span class ="' + className + '">' + (index + 1) + '</span>';
-//     // }
-//     // renderBullet: function (index, className)
-//     // {
-//     // return '<img class ="' + className + '" src="img/' + (index + 1) + '.svg" alt="">';
-//     // }
-
-//     /*
-//     type: 'fraction',
-//     //Custom
-//     renderFraction: function (currentClass, totalClass)
-//     {
-//       return 'Картинка <span class = "' + currentClass + '"></span>' + ' из ' + '<span class = "' + totalClass + '"></span>';
-//     }
-//     */
-//     /*
-//     type: 'progressbar'
-//     */
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//     draggable: true,
-//     cssMode:true
-//   },
-
-//   //Turn Touch grab function on PC
-//   simulateTouch: true,
-//   //Sensitivity of swipe
-//   touchRatio: 1,
-//   //Angle of working swipe
-//   touchAngle: 45,
-//   //Grab coursor
-//   grabCursor: true,
-  
-//   //Switching with click to slide
-//   slideToClicledSlide: true,
-
-//   //Navigation with hash
-//   hashNavigation:
-//   {
-//     //watch state of hash
-//     watchState: true,
-//   },
-
-//   //keyboard control
-//   keyboard:
-//   {
-//     //turn on/off
-//     enabled: true,
-//     //turn on/off only when slider in area of viewport
-//     onlyInViewport: true,
-//     //turn on/off control with pageUp/pageDown buttons on keyboard
-//     pageUpDown: true,
-//   },
-
-//   //mouse control
-//   mousewheel:
-//   {
-//     sensitivity: 1,
-//     //the object class on which this control wheel be able to use
-//     //P.S. Better to not use it if u got more then one swiper with this slider's name
-//     eventsTarget: ".swiper-slide"
-//   },
-
-//   autoHeight: false,
-
-//   // slidesPerView: 1,
-
-//   // slidesPerGroup: 1,
-
-//   centeredSlides: false,
-
-//   //if u need to make slides direction right to left
-//   //add for swiper container 'dir="rtl"'
-
-//   //before using it turn off autoHeight!!!
-//   //slidesPerColumn: 2,
-//   //add to js:
-//   /*
-//   .swiper
-//   {
-//     height: 700px;
-//   }
-//   .swiper .swiper-slide
-//   {
-//     height: calc((100% - 30px) / 2);
-//     overflow: hidden;
-//   }
-//   */
-
-//   //for better experience turn off autoHeight
-//   //freeMode: true,
-
-//   autoplay:
-//   {
-//     //pause between play
-//     delay: 1000,
-//     //end on last slide
-//     stopOnLastSlide: false,
-//     //turn off ofter interaction
-//     disableOnInteraction: false
-//   },
-
-//   speed: 500,
-
-//   // effect: 'fade',
-//   // fadeEffect:
-//   // {
-//   // //parallel fade changing
-//   // crossFade: true
-//   // },
-
-//   // effect: 'flip',
-//   // flipEffect:
-//   // {
-//   // slideShadows: true,
-//   // //show only active slide
-//   // limitRotation: true
-//   // },
-
-//   // effect: 'cube',
-//   // cubeEffect:
-//   // {
-//   // slideShadows: true,
-//   // shadow: true,
-//   // shadowOffset: 20,
-//   // shadowScale: 0.94
-//   // },
-
-//   // effect: 'coverflow',
-//   // coverflowEffect:
-//   // {
-//   // rotate: 20,
-//   // stretch: 50,
-//   // slideShadows: true,
-//   // },
-
-//   // effect: 'slide',
-
-//   // //adaptive
-//   breakpoints:
-//   {
-//     425:
-//     {
-//       slidesPerView: 1,
-//     },
-//     768:
-//     {
-//       slidesPerView: 2,
-//     },
-//     1024:
-//     {
-//       slidesPerView: 3,
-//       spaceBetween: 39  
-//     }
-//   }
-// });
